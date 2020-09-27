c3f2c99ede59322826eff50e6b156161a4e603cc
amirrezaask <raskarpour@gmail.com>
2020-09-27 20:25:27 +0330

    add some execute commands

--- lua/go/init.lua
+++ lua/go/init.lua
@@ -1,24 +1,39 @@
 local nvim = vim.api
 local go_fmt_command = 'go fmt'
 local go_get_command = 'go get'
+local go_install_command = 'go install'
 local go = {}
+
+local function go_run(file)
+    return function()
+        nvim.nvim_command(string.format('!go run %s', file))
+    end
+end
 
 go['go_fmt'] = function (pkg)
     nvim.nvim_command(string.format('silent ! %s %s', go_fmt_command, pkg))
     nvim.nvim_command('e')
 end
 
+go['go_run'] =  go_run("*.go")
+
 go['go_import'] = function(file)
     nvim.nvim_command(string.format('silent ! goimports -w %s', file))
+    nvim.nvim_command('e')
 end
+
 go['go_get'] = function (pkg)
     nvim.nvim_command(string.format('silent ! %s %s', go_get_command, pkg))
 end
 
-go['go_fmt_import_on_save'] = function ()
+go['enable_go_fmt_import_on_save'] = function ()
     nvim.nvim_command('autocmd BufWritePost *.go lua require\'go\'.go_import("%")')
     nvim.nvim_command('autocmd BufWritePost *.go lua require\'go\'.go_fmt(".")')
 end
 
+
+go['go_install'] = function()
+    nvim.nvim_command(string.format('silent ! %s .', go_install_command))
+end
 return go
 
--- plugin/go.vim
+++ plugin/go.vim
@@ -1,3 +1,5 @@
+command! GoInstall lua require'go'.go_install()
+command! GoRun lua require'go'.go_run()
 if exists("g:go_on_save_hooks")
-    lua require'go'.go_fmt_import_on_save()
+    lua require'go'.enable_go_fmt_import_on_save()
 endif
