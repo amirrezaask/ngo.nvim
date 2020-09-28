bc5ad913e9a387ac9b9f2a8d231dd90bd423d589
amirrezaask <raskarpour@gmail.com>
2020-09-28 15:07:21 +0330

    remove vimscript

--- lua/go/init.lua
+++ lua/go/init.lua
@@ -1,6 +1,5 @@
 local nvim = vim.api
 local go_fmt_command = 'go fmt'
-local go_get_command = 'go get'
 local go_install_command = 'go install'
 local go = {}
 
@@ -28,23 +27,33 @@ local function go_fmt(pkg)
         nvim.nvim_command('e')
     end
 end
+local function go_install(pkg)
+    return function()
+        nvim.nvim_command(string.format('silent ! %s %s', go_install_command, pkg))
+    end
+end
+local function enable_save_hooks()
+    nvim.nvim_command('autocmd BufWritePost *.go lua require\'go\'.go_import_this_file()')
+    nvim.nvim_command('autocmd BufWritePost *.go lua require\'go\'.go_fmt_this_pkg()')
+end
 
 go['go_fmt_this_pkg'] = go_fmt('.')
 go['go_test_all'] = go_test('./...')
 go['go_run'] =  go_run("*.go")
 go['go_import_this_file'] = go_import('%')
-go['go_get'] = function (pkg)
-    nvim.nvim_command(string.format('silent ! %s %s', go_get_command, pkg))
-end
+go['go_install'] = go_install('.')
+go['enable_go_fmt_import_on_save'] = enable_save_hooks
 
-go['enable_go_fmt_import_on_save'] = function ()
-    nvim.nvim_command('autocmd BufWritePost *.go lua require\'go\'.go_import_this_file()')
-    nvim.nvim_command('autocmd BufWritePost *.go lua require\'go\'.go_fmt_this_pkg()')
-end
+-- Define Ex commands
+nvim.nvim_command [[command! GoInstall lua require'go'.go_install()]]
+nvim.nvim_command[[command! GoRun lua require'go'.go_run()]]
+nvim.nvim_command[[command! GoTestAll lua require'go'.go_test_all()]]
+nvim.nvim_command[[command! GoImport lua require'go'.go_import()]]
+nvim.nvim_command[[command! GoFmt lua require'go'.go_fmt()]]
 
 
-go['go_install'] = function()
-    nvim.nvim_command(string.format('silent ! %s .', go_install_command))
-end
+-- Enable Go save hooks
+if nvim.nvim_get_var('go_on_save_hooks') then go['enable_go_fmt_import_on_save']() end
+
 return go
 
--- plugin/go.vim
+++
@@ -1,8 +0,0 @@
-command! GoInstall lua require'go'.go_install()
-command! GoRun lua require'go'.go_run()
-command! GoTestAll lua require'go'.go_test_all()
-command! GoImport lua require'go'.go_import()
-command! GoFmt lua require'go'.go_fmt()
-if exists("g:go_on_save_hooks")
-    lua require'go'.enable_go_fmt_import_on_save()
-endif
