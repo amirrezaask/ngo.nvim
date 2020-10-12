a65f7880c6bac0a25a56f707e29268b7bfc89862
amirrezaask <raskarpour@gmail.com>
2020-10-12 04:30:37 +0330

    fix tabs

--- lua/ngo/cmds.lua
+++ lua/ngo/cmds.lua
@@ -1,8 +1,8 @@
 return function()
-    vim.cmd [[command! GoInstall lua require'ngo.base'.utils.install()]]
-    vim.cmd [[command! GoRun lua require'ngo.base'.utils.run()]]
-    vim.cmd [[command! GoTestAll lua require'ngo.base'.utils.test_all()]]
-    vim.cmd [[command! GoImport lua require'ngo.base'.utils.imports()]]
-    vim.cmd [[command! GoFmt lua require'ngo.base'.utils.fmt()]]
-    vim.cmd [[command! GoTDD lua require'ngo.base'.utils.test_tdd()]]
+  vim.cmd [[command! GoInstall lua require'ngo.base'.utils.install()]]
+  vim.cmd [[command! GoRun lua require'ngo.base'.utils.run()]]
+  vim.cmd [[command! GoTestAll lua require'ngo.base'.utils.test_all()]]
+  vim.cmd [[command! GoImport lua require'ngo.base'.utils.imports()]]
+  vim.cmd [[command! GoFmt lua require'ngo.base'.utils.fmt()]]
+  vim.cmd [[command! GoTDD lua require'ngo.base'.utils.test_tdd()]]
 end
